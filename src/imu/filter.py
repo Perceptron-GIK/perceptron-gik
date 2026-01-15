58edb28ff219df4bbfe5352f4988e52e27ef5f75
Huiqi <huiqiguo.854@gmail.com>
2026-01-15 14:53:43 +0000

    Write filtered data to CSV files

--- src/imu/filter.py
+++ src/imu/filter.py
@@ -38,15 +38,21 @@ def kalman(raw, Q=1e-4, R=1e-3, x0=None, p0=1.0):
     return filtered
 
 ax_k, ay_k, az_k = kalman(ax_raw), kalman(ay_raw), kalman(az_raw)
+a_k = np.column_stack((t, ax_k, ay_k, az_k)) # Concatenate 3 axes into 1 array
+df_k = pd.DataFrame(a_k, columns=["t", "ax_k", "ay_k", "az_k"])
+df_k.to_csv("imu_kalman.csv", index=False) # Write filtered data to a CSV file
 
 # Savitzky-Golay filtering: window_length must be odd and >= polyorder + 2
 ax_sg = savgol_filter(ax_raw, window_length=11, polyorder=3)
 ay_sg = savgol_filter(ay_raw, window_length=11, polyorder=3)
 az_sg = savgol_filter(az_raw, window_length=11, polyorder=3)
+a_sg = np.column_stack((t, ax_sg, ay_sg, az_sg)) # Concatenate 3 axes into 1 array
+df_k = pd.DataFrame(a_k, columns=["t", "ax_sg", "ay_sg", "az_sg"])
+df_k.to_csv("imu_savgol.csv", index=False) # Write filtered data to a CSV file
 
-plt.plot(t, ax_raw, label="ax (raw)")
-plt.plot(t, ax_k, label="ax (kalman)")
-plt.plot(t, ax_sg, label="ax (savgol)")
+# plt.plot(t, ax_raw, label="ax (raw)")
+# plt.plot(t, ax_k, label="ax (kalman)")
+# plt.plot(t, ax_sg, label="ax (savgol)")
 
 # plt.plot(t, ay_raw, label="ay (raw)")
 # plt.plot(t, ay_k, label="ay (kalman)")
@@ -56,5 +62,5 @@ plt.plot(t, ax_sg, label="ax (savgol)")
 # plt.plot(t, az_k, label="az (kalman)")
 # plt.plot(t, az_sg, label="az (savgol)")
 
-plt.legend()
-plt.show()
+# plt.legend()
+# plt.show()
