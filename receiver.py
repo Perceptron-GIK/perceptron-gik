934c108fe1af908a8f73b90e31ffc0f184c4aba3
Jun Tan <tanjun8802@gmail.com>
2026-01-17 14:33:17 +0000

    PR modifications, remove duplications and spile accel and gyro

--- receiver.py
+++ receiver.py
@@ -4,215 +4,124 @@
 # The MTU is able to send 153 bytes in one packet, so all data is sent in one notification
 # The script utilises threading to run both left and right hand concurrently using asyncio.gather
 #------------------------------------------------------------------------------------------------------
-import asyncio, struct, time, threading
+import asyncio, struct, time
 from bleak import BleakScanner, BleakClient
 from datetime import datetime  
 
 DEVICE_NAME_L = "GIK_Nano_L" # Left hand nano name
 DEVICE_NAME_R = "GIK_Nano_R" # Right hand nano name
 
+UUID_TX_L = "00001235-0000-1000-8000-00805f9b34fb" # Left hand TX characteristic UUID
+UUID_TX_R = "00001237-0000-1000-8000-00805f9b34fb" # Right hand TX characteristic UUID
+
 packet_dtype_def = "<I" + "f"*6 + ("f"*6 + "B")*5  # little-endian
 assert struct.calcsize(packet_dtype_def) == 153 # match the packet size
 
 
 
-def left_handler(sender, data_left):
+def handler(side, data):
 
-    if len(data_left) != 153: # sanity check
-        print("Unexpected length from left hand", len(data_left))
+    if len(data) != 153: # sanity check
+        print(f"Unexpected length from {side} hand", len(data))
         return
     # unpack the packet
 
     (
         sample_id,
-        ax_l_base, ay_l_base, az_l_base,
-        gx_l_base, gy_l_base, gz_l_base,
+        ax_base, ay_base, az_base,
+        gx_base, gy_base, gz_base,
 
-        ax_l_thumb, ay_l_thumb, az_l_thumb,
-        gx_l_thumb, gy_l_thumb, gz_l_thumb, f_l_thumb_u8,
+        ax_thumb, ay_thumb, az_thumb,
+        gx_thumb, gy_thumb, gz_thumb, f_thumb_u8,
 
-        ax_l_index, ay_l_index, az_l_index,
-        gx_l_index, gy_l_index, gz_l_index, f_l_index_u8,
+        ax_index, ay_index, az_index,
+        gx_index, gy_index, gz_index, f_index_u8,
 
-        ax_l_middle, ay_l_middle, az_l_middle,
-        gx_l_middle, gy_l_middle, gz_l_middle, f_l_middle_u8,
+        ax_middle, ay_middle, az_middle,
+        gx_middle, gy_middle, gz_middle, f_middle_u8,
 
-        ax_l_ring, ay_l_ring, az_l_ring,
-        gx_l_ring, gy_l_ring, gz_l_ring, f_l_ring_u8,
+        ax_ring, ay_ring, az_ring,
+        gx_ring, gy_ring, gz_ring, f_ring_u8,
 
-        ax_l_pinky, ay_l_pinky, az_l_pinky,
-        gx_l_pinky, gy_l_pinky, gz_l_pinky, f_l_pinky_u8,
-    ) = struct.unpack(packet_dtype_def, data_left)
+        ax_pinky, ay_pinky, az_pinky,
+        gx_pinky, gy_pinky, gz_pinky, f_pinky_u8,
+    ) = struct.unpack(packet_dtype_def, data)
 
     # convert to Python bools with the same names
-    f_l_thumb  = bool(f_l_thumb_u8)
-    f_l_index  = bool(f_l_index_u8)
-    f_l_middle = bool(f_l_middle_u8)
-    f_l_ring   = bool(f_l_ring_u8)
-    f_l_pinky  = bool(f_l_pinky_u8)
+    f_thumb  = bool(f_thumb_u8)
+    f_index  = bool(f_index_u8)
+    f_middle = bool(f_middle_u8)
+    f_ring   = bool(f_ring_u8)
+    f_pinky  = bool(f_pinky_u8)
 
     t = time.time()
     ts_string = datetime.fromtimestamp(t).strftime("%H:%M:%S.%f")[:-3]  # HH:MM:SS.mmm
 
     print(
-        f"id={sample_id} "
+        f"id={sample_id},{side} "
         f"time={ts_string} "
-        f"a_l_b=({ax_l_base:.4f},{ay_l_base:.4f},{az_l_base:.4f}) "
-        f"g_l_b=({gx_l_base:.4f},{gy_l_base:.4f},{gz_l_base:.4f}) "
-        f"a_l_t=({ax_l_thumb:.4f},{ay_l_thumb:.4f},{az_l_thumb:.4f}) "
-        f"g_l_t=({gx_l_thumb:.4f},{gy_l_thumb:.4f},{gz_l_thumb:.4f}) "
-        f"f_l_t={int(f_l_thumb)} "
-        f"a_l_i=({ax_l_index:.4f},{ay_l_index:.4f},{az_l_index:.4f}) "
-        f"g_l_i=({gx_l_index:.4f},{gy_l_index:.4f},{gz_l_index:.4f}) "
-        f"f_l_i={int(f_l_index)} "
-        f"a_l_m=({ax_l_middle:.4f},{ay_l_middle:.4f},{az_l_middle:.4f}) "
-        f"g_l_m=({gx_l_middle:.4f},{gy_l_middle:.4f},{gz_l_middle:.4f}) "
-        f"f_l_m={int(f_l_middle)} "
-        f"a_l_r=({ax_l_ring:.4f},{ay_l_ring:.4f},{az_l_ring:.4f}) "
-        f"g_l_r=({gx_l_ring:.4f},{gy_l_ring:.4f},{gz_l_ring:.4f}) "
-        f"f_l_r={int(f_l_ring)} "
-        f"a_l_p=({ax_l_pinky:.4f},{ay_l_pinky:.4f},{az_l_pinky:.4f}) "
-        f"g_l_p=({gx_l_pinky:.4f},{gy_l_pinky:.4f},{gz_l_pinky:.4f}) "
-        f"f_l_p={int(f_l_pinky)}"
-    )
-
-def right_handler(sender, data_right):
-
-    if len(data_right) != 153: # sanity check
-        print("Unexpected length from right hand", len(data_right))
-        return
-    # unpack the packet
-
-    (
-        sample_id,
-        ax_r_base, ay_r_base, az_r_base,
-        gx_r_base, gy_r_base, gz_r_base,
-
-        ax_r_thumb, ay_r_thumb, az_r_thumb,
-        gx_r_thumb, gy_r_thumb, gz_r_thumb, f_r_thumb_u8,
-
-        ax_r_index, ay_r_index, az_r_index,
-        gx_r_index, gy_r_index, gz_r_index, f_r_index_u8,
-
-        ax_r_middle, ay_r_middle, az_r_middle,
-        gx_r_middle, gy_r_middle, gz_r_middle, f_r_middle_u8,
-
-        ax_r_ring, ay_r_ring, az_r_ring,
-        gx_r_ring, gy_r_ring, gz_r_ring, f_r_ring_u8,
-
-        ax_r_pinky, ay_r_pinky, az_r_pinky,
-        gx_r_pinky, gy_r_pinky, gz_r_pinky, f_r_pinky_u8,
-    ) = struct.unpack(packet_dtype_def, data_right)
-
-    # convert to Python bools with the same names
-    f_r_thumb  = bool(f_r_thumb_u8)
-    f_r_index  = bool(f_r_index_u8)
-    f_r_middle = bool(f_r_middle_u8)
-    f_r_ring   = bool(f_r_ring_u8)
-    f_r_pinky  = bool(f_r_pinky_u8)
-
-    t = time.time()
-    ts_string = datetime.fromtimestamp(t).strftime("%H:%M:%S.%f")[:-3]  # HH:MM:SS.mmm
-
-    print(
-        f"id={sample_id} "
-        f"time={ts_string} "
-        f"a_r_b=({ax_r_base:.4f},{ay_r_base:.4f},{az_r_base:.4f}) "
-        f"g_r_b=({gx_r_base:.4f},{gy_r_base:.4f},{gz_r_base:.4f}) "
-        f"a_r_t=({ax_r_thumb:.4f},{ay_r_thumb:.4f},{az_r_thumb:.4f}) "
-        f"g_r_t=({gx_r_thumb:.4f},{gy_r_thumb:.4f},{gz_r_thumb:.4f}) "
-        f"f_r_t={int(f_r_thumb)} "
-        f"a_r_i=({ax_r_index:.4f},{ay_r_index:.4f},{az_r_index:.4f}) "
-        f"g_r_i=({gx_r_index:.4f},{gy_r_index:.4f},{gz_r_index:.4f}) "
-        f"f_r_i={int(f_r_index)} "
-        f"a_r_m=({ax_r_middle:.4f},{ay_r_middle:.4f},{az_r_middle:.4f}) "
-        f"g_r_m=({gx_r_middle:.4f},{gy_r_middle:.4f},{gz_r_middle:.4f}) "
-        f"f_r_m={int(f_r_middle)} "
-        f"a_r_r=({ax_r_ring:.4f},{ay_r_ring:.4f},{az_r_ring:.4f}) "
-        f"g_r_r=({gx_r_ring:.4f},{gy_r_ring:.4f},{gz_r_ring:.4f}) "
-        f"f_r_r={int(f_r_ring)} "
-        f"a_r_p=({ax_r_pinky:.4f},{ay_r_pinky:.4f},{az_r_pinky:.4f}) "
-        f"g_r_p=({gx_r_pinky:.4f},{gy_r_pinky:.4f},{gz_r_pinky:.4f}) "
-        f"f_r_p={int(f_r_pinky)}"
+        f"a_b=({ax_base:.4f},{ay_base:.4f},{az_base:.4f}) "
+        f"g_b=({gx_base:.4f},{gy_base:.4f},{gz_base:.4f}) "
+        f"a_t=({ax_thumb:.4f},{ay_thumb:.4f},{az_thumb:.4f}) "
+        f"g_t=({gx_thumb:.4f},{gy_thumb:.4f},{gz_thumb:.4f}) "
+        f"f_t={int(f_thumb)} "
+        f"a_i=({ax_index:.4f},{ay_index:.4f},{az_index:.4f}) "
+        f"g_i=({gx_index:.4f},{gy_index:.4f},{gz_index:.4f}) "
+        f"f_i={int(f_index)} "
+        f"a_m=({ax_middle:.4f},{ay_middle:.4f},{az_middle:.4f}) "
+        f"g_m=({gx_middle:.4f},{gy_middle:.4f},{gz_middle:.4f}) "
+        f"f_m={int(f_middle)} "
+        f"a_r=({ax_ring:.4f},{ay_ring:.4f},{az_ring:.4f}) "
+        f"g_r=({gx_ring:.4f},{gy_ring:.4f},{gz_ring:.4f}) "
+        f"f_r={int(f_ring)} "
+        f"a_p=({ax_pinky:.4f},{ay_pinky:.4f},{az_pinky:.4f}) "
+        f"g_p=({gx_pinky:.4f},{gy_pinky:.4f},{gz_pinky:.4f}) "
+        f"f_p={int(f_pinky)}"
     )
 
 
-async def wait_for_left_nano():
-    nano_left = None
-    while nano_left is None:
+async def wait_for_nano(device_name):
+    nano = None
+    while nano is None:
         devices = await BleakScanner.discover()
         for d in devices:
-            if d.name == DEVICE_NAME_L:
-                nano_left = d
+            if d.name == device_name:
+                nano = d
                 break
-        if nano_left is None:
+        if nano is None:
             await asyncio.sleep(0.5)  # wait 0.5s then scan again
-    print("Found GIK Left Hand")
-    print(nano_left)
-    return nano_left
-
-async def wait_for_right_nano():
-    nano_right = None
-    while nano_right is None:
-        devices = await BleakScanner.discover()
-        for e in devices:
-            if e.name == DEVICE_NAME_R:
-                nano_right = e
-                break
-        if nano_right is None:
-            await asyncio.sleep(0.5)  # wait 0.5s then scan again
-    print("Found GIK Right Hand")
-    print(nano_right)
-    return nano_right
+    print(nano)
+    return nano
 
 
-async def left():
-    nano_left = await wait_for_left_nano()
+async def connect(device_name, uuid):
 
+    # To distingiush left and right from the device name
+    if "_R" in device_name:
+        side = "Right"
+    elif "_L" in device_name:
+        side = "Left"
+
+    nano = await wait_for_nano(device_name)
+    print(f"Found GIK {side} Hand")
     while True:
 
-        async with BleakClient(nano_left.address) as client:
-            tx_char_left = None
-            print("Connected:", nano_left.address)
+        async with BleakClient(nano.address) as client:
+            tx_chareft = None
+            print("Connected:", nano.address)
             for service in client.services:
                 print("Service:", service.uuid)
                 for char in service.characteristics:
                 # Find the char object with that UUID once, then reuse it, did this to avoid string format not matching
-                    if str(char.uuid) == "00001235-0000-1000-8000-00805f9b34fb": # UUID for Left hand tx characteristic
-                        tx_char_left = char
+                    if str(char.uuid) == uuid: # UUID for Left hand tx characteristic
+                        tx_char = char
                         break
-                if tx_char_left:
+                if tx_char:
                     break
 
-            print("Left TX characteristic:", tx_char_left)
+            print(f"{side} Characteristic:", tx_char)
 
-            await client.start_notify(tx_char_left, left_handler) # Activate notifications/indications on the characteristic.
-
-            while client.is_connected:
-                
-                # Match your Nano loop rate (100Hz)
-                await asyncio.sleep(0.01)
-
-async def right():
-    nano_right = await wait_for_right_nano()
-
-    while True:
-        async with BleakClient(nano_right.address) as client:
-            tx_char_right = None
-            print("Connected:", nano_right.address)
-            for service in client.services:
-                print("Service:", service.uuid)
-                for char in service.characteristics:
-                # Find the char object with that UUID once, then reuse it, did this to avoid string format not matching
-                    if str(char.uuid) == "00001237-0000-1000-8000-00805f9b34fb": # UUID for Right hand tx characteristic
-                        tx_char_right = char
-                        break
-                if tx_char_right:
-                    break
-
-            print("Right TX characteristic:", tx_char_right)
-
-            await client.start_notify(tx_char_right, right_handler) # Activate notifications/indications on the characteristic.
+            await client.start_notify(tx_char, handler(side)) # Activate notifications/indications on the characteristic.
 
             while client.is_connected:
                 
@@ -223,7 +132,7 @@ async def right():
 
 async def main():
     print(f"Waiting for GIK to appear...")
-    await asyncio.gather(left(), right()) # Run both left and right concurrently, asyncio handles the threading
+    await asyncio.gather(connect(DEVICE_NAME_L, UUID_TX_L), connect(DEVICE_NAME_R, UUID_TX_R)) # Run both left and right concurrently, asyncio handles the threading
 
 
 asyncio.run(main())
