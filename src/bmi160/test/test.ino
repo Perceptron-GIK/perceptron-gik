a91c99387186fda4ccce75c917b741faa9b6ca41
Huiqi <huiqiguo.854@gmail.com>
2026-01-22 15:57:11 +0000

    Change IMU connection from SPI to I2C

--- src/bmi160/test/test.ino
+++ src/bmi160/test/test.ino
@@ -1,83 +1,42 @@
-#include <SPI.h>
-#include <Adafruit_BMP280.h>
 #include <BMI160Gen.h>
+#include <Wire.h>
 
-const int select_pin = 10;
-Adafruit_BMP280 BMP280;
+const int i2c_addr = 0x68; 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Initialising BMI160...");
-  while (!Serial); 
+  while (!Serial);
 
-  SPI.begin();
-  BMI160.begin(select_pin);
-  uint8_t dev_id = BMI160.getDeviceID();
-  Serial.print("DEVICE ID: ");
-  Serial.println(dev_id, HEX);
+  Wire.begin();
 
-  BMI160.setGyroRange(250);
-  BMI160.setAccelerometerRange(4);
+  if (!BMI160.begin(BMI160GenClass::I2C_MODE, i2c_addr)) {
+    Serial.println("BMI160 initialization failed!");
+    while (1);
+  }
+
+  Serial.println("BMI160 initialized successfully in I2C mode!");
 }
 
 void loop() {
-  int axRaw, ayRaw, azRaw, gxRaw, gyRaw, gzRaw, mxRaw, myRaw, mzRaw, tRaw;
-  float ax, ay, az, gx, gy, gz;
+  int gx, gy, gz;
+  int ax, ay, az;
 
-  BMI160.readMotionSensor9(axRaw, ayRaw, azRaw, gxRaw, gyRaw, gzRaw, mxRaw, myRaw, mzRaw, tRaw);
-  
-  gx = convertRawGyro(gxRaw);
-  gy = convertRawGyro(gyRaw);
-  gz = convertRawGyro(gzRaw);
+  BMI160.readGyro(gx, gy, gz);
+  BMI160.readAccelerometer(ax, ay, az);
 
-  ax = convertRawAccel(axRaw);
-  ay = convertRawAccel(ayRaw);
-  az = convertRawAccel(azRaw);
-
-  Serial.print("a:\t");
-  Serial.print(ax);
-  Serial.print("\t");
-  Serial.print(ay);
-  Serial.print("\t");
-  Serial.print(az);
-  Serial.println();
-  Serial.print("g:\t");
+  Serial.print("Gyroscope Data (X, Y, Z): ");
   Serial.print(gx);
-  Serial.print("\t");
+  Serial.print(", ");
   Serial.print(gy);
-  Serial.print("\t");
-  Serial.print(gz);
-  Serial.println();
-  Serial.print("m:\t");
-  Serial.print(mxRaw);
-  Serial.print("\t");
-  Serial.print(myRaw);
-  Serial.print("\t");
-  Serial.print(mzRaw);
-  Serial.println();
-  Serial.print(tRaw);
-  Serial.println();
-  Serial.println();
+  Serial.print(", ");
+  Serial.println(gz);
 
-  delay(500);
-}
+  Serial.print("Accelerometer Data (X, Y, Z): ");
+  Serial.print(ax);
+  Serial.print(", ");
+  Serial.print(ay);
+  Serial.print(", ");
+  Serial.println(az);
 
-float convertRawGyro(int gRaw) {
-  // since we are using 250 degrees/seconds range
-  // -250 maps to a raw value of -32768
-  // +250 maps to a raw value of 32767
-
-  float g = (gRaw * 250.0) / 32768.0;
-
-  return g;
-}
-
-float convertRawAccel(int aRaw) {
-  // since we are using 250 degrees/seconds range
-  // -250 maps to a raw value of -32768
-  // +250 maps to a raw value of 32767
-
-  float a = (aRaw * (4/32768.0));
-
-  return a;
+  delay(100);
 }
