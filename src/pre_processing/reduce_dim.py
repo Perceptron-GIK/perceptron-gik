730655e6570990cf9c90b9d86f0df7a94b456d4c
Huiqi <huiqiguo.854@gmail.com>
2026-02-20 12:17:34 +0000

    Checkpoint: Keep only active IMU data + active finger index

--- src/pre_processing/reduce_dim.py
+++ src/pre_processing/reduce_dim.py
@@ -29,9 +29,25 @@ def active_imu_only(data_dir, has_left, has_right):
     
     if has_left and has_right:
         fsr_indices = torch.tensor([12, 19, 26, 33, 40, 71, 78, 85, 92, 99])
+        base_indices = torch.tensor([0, 1, 2, 3, 4, 5, 41, 42, 43, 59, 60, 61, 62, 63, 64, 100, 101, 102])
     else:
         fsr_indices = torch.tensor([12, 19, 26, 33, 40])
+        base_indices = torch.tensor([0, 1, 2, 3, 4, 5, 41, 42, 43])
     
-    fsr_data = samples[:, :, fsr_indices]
-    for window in fsr_data:
-        fsr = torch.argmax((window != 0).sum(dim=0)) # Identify the FSR that was pressed
+    W, R, C = samples.shape # (nWindows, nRows, nCols), for reference only
+    fsr_data = samples[:, :, fsr_indices] # (W, R, C), extract the columns with FSR data
+    active_finger = (fsr_data != 0).sum(dim=1).argmax(dim=1) # (W, 1), identify the active finger which is an index from 0-9
+    active_fsr = fsr_indices[active_finger].unsqueeze(1) # (W, 1), identify the feature index of the active FSR
+
+    offsets = torch.arange(6).unsqueeze(0) # (1, 6)
+    cols_to_keep = active_fsr - 6 + offsets # (W, 6), keep the 6 columns of active IMU data from each window
+    cols_to_keep = cols_to_keep.unsqueeze(1).expand(-1, R, -1) # (W, R, 6), reshape to match data dimensions
+    output = torch.gather(samples, dim=2, index=cols_to_keep) # (W, R, 6), gather the active IMU data from each window
+    
+    # Shift the right hand finger indices from 0-4 to 5-9 
+    finger_idx = (active_finger if (has_left and has_right) or (has_left and not has_right) else active_finger + 5).unsqueeze(1) # (W, 1)
+
+    # Insert a feature indicating the active finger, which is an index from 0-9
+    finger_feature = finger_idx.unsqueeze(1).expand(-1, R, 1) # (W, R, 1), reshape to match data dimensions
+    output = torch.cat([finger_feature, output], dim=2) # (W, R, 7)
+    print(output)
