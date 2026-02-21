0c32bb77b16fe8b88c8c4e821c3068de36098f15
Huiqi <huiqiguo.854@gmail.com>
2026-02-21 00:40:22 +0000

    Include predicted positions for active finger IMU

--- src/pre_processing/reduce_dim.py
+++ src/pre_processing/reduce_dim.py
@@ -44,21 +44,23 @@ def active_imu_only(data_dir, has_left, has_right, normalise, output_path):
     if has_left and has_right:
         fsr_indices = torch.tensor([12, 19, 26, 33, 40, 71, 78, 85, 92, 99])
         base_indices = torch.tensor([0, 1, 2, 3, 4, 5, 41, 42, 43, 59, 60, 61, 62, 63, 64, 100, 101, 102])
+        pos_ref = torch.tensor([44, 103]) # Starting index of predicted positions for finger IMUs
     else:
         fsr_indices = torch.tensor([12, 19, 26, 33, 40])
         base_indices = torch.tensor([0, 1, 2, 3, 4, 5, 41, 42, 43])
+        pos_ref = torch.tensor([44])
     
-    W, R, C = samples.shape # (nWindows, nRows, nCols), for reference only
+    W, R, C = samples.shape # (nWindows, nRows, nCols)
     fsr_data = samples[:, :, fsr_indices] # (W, R, C), extract the columns with FSR data
     active_finger = (fsr_data != 0).sum(dim=1).argmax(dim=1) # (W, 1), identify the active finger which is an index from 0-9
     active_fsr = fsr_indices[active_finger].unsqueeze(1) # (W, 1), identify the feature index of the active FSR
 
-    offsets = torch.arange(6).unsqueeze(0) # (1, 6)
-    cols_to_keep = active_fsr - 6 + offsets # (W, 6), keep the 6 columns of active IMU data from each window
+    fsr_offsets = torch.arange(6).unsqueeze(0) # (1, 6)
+    cols_to_keep = active_fsr - 6 + fsr_offsets # (W, 6), keep the 6 columns of active IMU data from each window
     cols_to_keep = cols_to_keep.unsqueeze(1).expand(-1, R, -1) # (W, R, 6), reshape to match data dimensions
     output = torch.gather(samples, dim=2, index=cols_to_keep) # (W, R, 6), gather the active IMU data from each window
     
-    # Shift the right hand finger indices from 0-4 to 5-9 
+    # If right hand only, shift the right hand finger indices from 0-4 to 5-9 
     finger_idx = (active_finger if (has_left and has_right) or (has_left and not has_right) else active_finger + 5).unsqueeze(1) # (W, 1)
 
     # Insert a feature indicating the active finger, which is an index from 0-9
@@ -68,6 +70,19 @@ def active_imu_only(data_dir, has_left, has_right, normalise, output_path):
     # Insert data from base IMU
     base_data = samples[:, :, base_indices]
     output = torch.cat([output, base_data], dim=2)
+
+    # If both hands, shift the right hand finger indices from 5-9 to 0-4
+    active_finger = torch.where((has_left and has_right) & (active_finger >= 5), active_finger - 5, active_finger).unsqueeze(1) # (W, 1)
+
+    # Insert predicted positions for active finger IMU
+    pos_start = (active_finger*3).unsqueeze(1) # (W, 1, 1), starting index of predicted position for the active finger
+    pos_offsets = torch.arange(3).unsqueeze(0).unsqueeze(0) # (1, 1, 3), pos xyz
+    pos_ref = pos_ref.unsqueeze(0).unsqueeze(2) # (1, nHands, 1)
+    pos_indices = pos_start + pos_ref + pos_offsets # (W, nHands, 3)
+    pos_indices = pos_indices.reshape(W, -1) # (W, nHands*3)
+    pos_indices = pos_indices.unsqueeze(1).expand(-1, R, -1) # (W, R, nHands*3)
+    pos_data = torch.gather(samples, dim=2, index=pos_indices) # (W, R, nHands*3)
+    output = torch.cat([output, pos_data], dim=2)
 
     # Compute normalisation stats
     mean, std = None, None
