b763d69eb47553c637d9b0083a28638651bc4b2f
Souparna Mandal <souparna2003@gmail.com>
2026-02-22 11:42:35 +0000

    testing variants of coordinate loss

--- ml/models/loss_functions/custom_losses.py
+++ ml/models/loss_functions/custom_losses.py
@@ -33,9 +33,36 @@ class CoordinateLoss(nn.Module):
         """
         super().__init__()
         self.ratio = h_v_ratio
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
     
     def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        loss = F.mse_loss(inputs, targets, reduction="none") # reduction prevents aggregation of the last axis
+        x = self.relu(inputs)
+        loss = F.mse_loss(x, targets, reduction="none") # reduction prevents aggregation of the last axis
+        weights = loss.new_tensor([self.ratio, 1]) # first logit is horizontal
+        weighted_loss = weights * loss
+        
+        return weighted_loss.mean()
+    
+    
+class CoordinateLossClassification(nn.Module):
+    """Focal Loss for handling class imbalance. Best performance with gamma=2.0."""
+    
+    def __init__(self, h_v_ratio):
+        """Params for the Coordinate Loss that takes two continuous logits as inputs and measurers their coordinate differences to the target
+
+        Args:
+            h_v_ratio (float): ratio of the weights given to horizontal correctness vs vertical correctness. 
+            i.e, 1.5 means horizontal correctness matters 1.5 times more than vertical correctness
+        """
+        super().__init__()
+        self.sigmoid = nn.Sigmoid()
+        self.ratio = h_v_ratio
+    
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        x = 9 *  self.sigmoid(inputs)
+        x = torch.floor(x)
+        loss = F.mse_loss(x, targets, reduction="none") # reduction prevents aggregation of the last axis
         weights = loss.new_tensor([self.ratio, 1]) # first logit is horizontal
         weighted_loss = weights * loss
         
