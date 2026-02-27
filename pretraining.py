Fix bug with normalisation not being applied

pretraining.py
@@ -187,10 +187,8 @@ def preprocess_multiple_sources(
             # per-feature min, max for non-FSR features
             x_min = all_data.amin(dim=0)              # [F]
             x_max = all_data.amax(dim=0)              # [F]
-            mean = all_data.mean(dim=0)                    # [F]        
-            std = all_data.std(dim=0)                      # [F] 
-            print(x_min, x_max)  
-            
+            mean = all_data.mean(dim=0)               # [F]        
+            std = all_data.std(dim=0)                 # [F] 
 
             # avoid zero range
             range_ = x_max - x_min
@@ -209,20 +207,20 @@ def preprocess_multiple_sources(
             s = torch.nan_to_num(s, nan=0.0, posinf=0.0, neginf=0.0)
             s_norm = s.clone()
             mask_valid = (s.abs().sum(dim=1) > 0)
+            valid_idx = mask_valid.nonzero(as_tuple=True)[0]
 
             # map non-FSR features to [-1, 1]
             # formula: 2 * (x - min)/(max - min) - 1
-            s_norm_valid = s_norm[mask_valid]
-            s_valid = s[mask_valid]
+            if len(valid_idx) > 0:
+                # normalize non-FSR
+                s_norm[valid_idx[:, None], nonfsr] = 2.0 * (
+                    (s[valid_idx[:, None], nonfsr] - x_min[nonfsr]) / range_[nonfsr]
+                ) - 1.0
 
-            s_norm_valid[:, nonfsr] = 2.0 * (
-                (s_valid[:, nonfsr] - x_min[nonfsr]) / range_[nonfsr]
-            ) - 1.0
+                # copy FSR as-is
+                s_norm[valid_idx[:, None], fsr] = s[valid_idx[:, None], fsr]
 
-
-            s_norm[mask_valid][:, fsr] = s[mask_valid][:, fsr]
             s_norm[~mask_valid] = 0.0
-
             norm_samples.append(s_norm)
 
         samples_tensor = norm_samples