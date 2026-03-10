80467409fde0fea5dfcc088c0450270ea038860b
Huiqi <huiqiguo.854@gmail.com>
2026-03-10 13:45:10 +0000

    Incorporate autocorrect and inference eval

--- inference_receiver.py
+++ inference_receiver.py
@@ -1,10 +1,11 @@
-import asyncio, struct, time, os, sys, yaml, torch
+import asyncio, struct, time, os, sys, yaml, torch, re
 import numpy as np
 from typing import Callable, Any, Optional, Dict, List
 from bleak import BleakScanner, BleakClient
 
 from src.Constants.char_to_key import NUM_CLASSES, INDEX_TO_CHAR, CHAR_TO_INDEX, FULL_COORDS
 from src.inference.sliding_window import SlidingWindow
+from src.inference.autocorrect import AutoCorrector
 from src.visualisation.visualisation import get_closest_coordinate
 from src.decoding.lm_fusion import (
     build_char_ngram_lm,
@@ -44,7 +45,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath('__file__'))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-TRAINING_CONFIG_PATH = os.path.join(PROJECT_ROOT, "train_config.yaml")
+TRAINING_CONFIG_PATH = os.path.join(PROJECT_ROOT, "train_config_positionoffset_3layerinner.yaml")
 with open(TRAINING_CONFIG_PATH, "r", encoding="utf-8") as f:
     config_data = yaml.safe_load(f)
 
@@ -99,11 +100,16 @@ TTA_SCALE_JITTER = float(TRAIN_CFG.get("lm_tta_scale_jitter", 0.0))
 _INFERENCE_LM: Optional[dict] = None
 _INFERENCE_MODEL: Optional[torch.nn.Module] = None
 
-MODEL_PATH = os.path.join(PROJECT_ROOT, "best_model.pt")
+MODEL_PATH = os.path.join(PROJECT_ROOT, "gik_model_positionoffset_3layerinner.pt")
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 
 FSR_INDICES = [12, 19, 26, 33, 40]
+
+AUTOCORRECTOR = AutoCorrector(checker_type="pyspell", max_len=10)
+
+GROUND_TRUTH = "hello world how are you this is our invisible keyboard"
+inference_predictions = ""
 
 ## ================================================== ##
 # BLUETOOTH FUNCTIONS
@@ -273,12 +279,16 @@ def run_inference(
             if lm is not None and LM_INFERENCE_BETA > 0.0:
                 history = list(history_chars)[-LM_HISTORY_LEN:] if history_chars else []
                 logits = fuse_single_step_logits_with_lm(logits, lm, history, LM_INFERENCE_BETA)
-            prediction = logits.argmax(dim=-1).item()
+            predicted_idx = logits.argmax(dim=-1).item()
         else:
-            prediction = CHAR_TO_INDEX[get_closest_coordinate(model.predict_coords(x.to(DEVICE)), FULL_COORDS)]
+            predicted_idx = CHAR_TO_INDEX[get_closest_coordinate(model.predict_coords(x.to(DEVICE)), FULL_COORDS)]
 
-    print(INDEX_TO_CHAR[prediction], end="")
-    return prediction
+    predicted_char = INDEX_TO_CHAR[predicted_idx]
+    inference_predictions += predicted_char
+    # AUTOCORRECTOR.process_char(predicted_char)
+    sys.stdout.write(predicted_char)
+    sys.stdout.flush()
+    return predicted_idx
     
 async def process_queues(left_queue, right_queue):
     left_win = SlidingWindow()
@@ -303,6 +313,12 @@ async def process_queues(left_queue, right_queue):
             end_abs_idx = min(max_abs_idx, decoded_abs_idx + context_future)
             start_i = start_abs_idx - event_offset
             end_i = end_abs_idx - event_offset
+
+            # left_pointer = np.vstack([events[j]["left"] for j in range(start_i, end_i + 1)]).shape[0]
+            # right_pointer = np.vstack([events[j]["right"] for j in range(start_i, end_i + 1)]).shape[0]
+
+            # left_ctx = np.vstack([events[j]["left"] for j in range(end_i + 1)])
+            # right_ctx = np.vstack([events[j]["right"] for j in range(end_i + 1)])
 
             left_ctx = np.vstack([events[j]["left"] for j in range(start_i, end_i + 1)])
             right_ctx = np.vstack([events[j]["right"] for j in range(start_i, end_i + 1)])
@@ -330,7 +346,6 @@ async def process_queues(left_queue, right_queue):
             [left_task, right_task],
             return_when=asyncio.FIRST_COMPLETED
         )
-
         for task in completed:
             data, t = task.result()
             data = np.asarray(data, dtype=np.float32)
@@ -365,6 +380,19 @@ async def process_queues(left_queue, right_queue):
             events.append({"left": opp_chunk, "right": chunk})
         await decode_ready_events()
 
+def evaluate_inference(ground_truth, predictions):
+    predictions = re.sub("\b", "", predictions)
+    error, correct = 0, 0
+    seq_length = min(len(ground_truth), len(predictions))
+    for i in range(seq_length):
+        if ground_truth[i] == predictions[i]:
+            correct += 1
+        gt_coords = FULL_COORDS[ground_truth[i]]
+        pred_coords = FULL_COORDS[predictions[i]]
+        error += (gt_coords[0] - pred_coords[0])**2 + (gt_coords[1] - pred_coords[1])**2
+    acc = round((correct/seq_length)**100, 2)
+    return error, acc
+
 ## ================================================== ##
 
 async def main():
@@ -373,8 +401,12 @@ async def main():
     data_queue_left = asyncio.Queue(MAX_QUEUE_SIZE)
     data_queue_right = asyncio.Queue(MAX_QUEUE_SIZE)
 
-    await asyncio.gather(connect(DEVICE_NAME_L, UUID_TX_L, data_queue_left), 
-                         connect(DEVICE_NAME_R, UUID_TX_R, data_queue_right),
-                         process_queues(data_queue_left, data_queue_right))
-
+    try:
+        await asyncio.gather(connect(DEVICE_NAME_L, UUID_TX_L, data_queue_left), 
+                                connect(DEVICE_NAME_R, UUID_TX_R, data_queue_right),
+                                process_queues(data_queue_left, data_queue_right))
+    finally:
+            error, acc = evaluate_inference(GROUND_TRUTH, inference_predictions)
+            print(f"Inference Error: {error}, Inference Accuracy: {acc}%")
+    
 asyncio.run(main())
